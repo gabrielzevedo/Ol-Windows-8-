43fd4cc32c905629da40d849b51bc7b10246b72a
Gabriel Azevedo <gabrielx86@live.com>
2017-10-29 01:54:57 -0200

    Add files via upload

---
+++ pages/itemDetail/itemDetail.js
@@ -0,0 +1,16 @@
+﻿(function () {
+    "use strict";
+
+    WinJS.UI.Pages.define("/pages/itemDetail/itemDetail.html", {
+        // This function is called whenever a user navigates to this page. It
+        // populates the page elements with the app's data.
+        ready: function (element, options) {
+            var item = options && options.item ? Data.resolveItemReference(options.item) : Data.items.getAt(0);
+            element.querySelector(".titlearea .pagetitle").textContent = item.group.title;
+            element.querySelector("article .item-title").textContent = item.title;
+            element.querySelector("article .item-subtitle").textContent = item.subtitle;
+            element.querySelector("article .item-content").innerHTML = item.content;
+            element.querySelector(".content").focus();
+        }
+    });
+})();
